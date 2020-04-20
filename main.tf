3aaac6f7d1ad19ca688d2d67d67b6f826e3bfd1f
Tas Damen <tasdamen@cap-rx.com>
2020-04-19 20:55:51 -0400

    adding overriden variables

--- main.tf
+++ main.tf
@@ -32,7 +32,9 @@ resource "aws_s3_bucket" "terraform_state" {
 
 module "web_server" {
   source = "./modules/web-server"
-  
+  server_port = var.server_port
+  server_name = var.server_name
+  security_group_name = var.security_group_name
 }
 
 
